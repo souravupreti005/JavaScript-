6d87ca31cc4e17d9f6337d403aa52e293b27dd9d
souravupreti005 <166937659+souravupreti005@users.noreply.github.com>
2024-04-14 03:37:14 +0000

    conainer done

---
+++ day_01/test.js
@@ -0,0 +1,17 @@
+/*const 
+let 
+var */
+
+// use const ,let 
+
+
+const name="sourav" //the value of name doesn't change 
+let phoneNumber=93547434522 //the value of phone number can be changed 
+let address; // initially the value is undefined
+
+
+console.log(name)           //conaole.log() is use for print the output 
+console.log(phoneNumber)
+
+
+console.table([name,phoneNumber,address]) //it is also use for print the output in a table form 
