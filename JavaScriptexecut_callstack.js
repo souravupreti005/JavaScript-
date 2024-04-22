f08a063483b9cbb0b857be45dc576a5947886da8
souravupreti005 <166937659+souravupreti005@users.noreply.github.com>
2024-04-22 14:51:14 +0000

    javascript execution

---
+++ JavaScriptexecut_callstack.js
@@ -0,0 +1,13 @@
+// javascript execution contest 
+// 1.global execution contest -----> this
+// 2.function execution contest
+// 3.eval execution contest
+
+
+
+// code----->>>> 1.memory creation phase
+//               2. execution phase
+
+
+//new enviroment variable 
+//thread 
